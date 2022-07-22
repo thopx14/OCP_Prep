e0f572ffdc90d03afabbf8621eaee6ed8547577d
Thomas <thopx@proton.me>
2022-07-22 11:37:07 +0200

    Creating a local Comparator<OS> instead of an static inner class.

--- src/ClassDesign/Aufgaben/OS.java
+++ src/ClassDesign/Aufgaben/OS.java
@@ -53,26 +53,18 @@ public class OS implements Comparable<OS> {
         return cmp;
     }
 
-    static class OSComparator implements Comparator<OS> {
 
-        @Override
-        public int compare( OS o1, OS o2 ) {
-            int cmp = o1.getName().compareTo( o2.getName() );
+    public static void main( String[] args ) {
+    	
+        Comparator<OS> cmpNatural = (o1, o2) -> {
+        	int cmp = o1.getName().compareTo( o2.getName() );
             if ( cmp == 0 ) {
                 cmp = Integer.compare( o1.getVersion(), o2.getVersion() );
             }
             return cmp;
-        }
-
-        @Override
-        public Comparator<OS> reversed() {
-            return Comparator.super.reversed();
-        }
-
-    }
-
-    public static void main( String[] args ) {
-        OS[] osses = {
+        };
+    	
+    	OS[] osses = {
                 new OS( "Linux", 3 ),
                 new OS( "Windows", 95 ),
                 new OS( "MAC", 9 ),
@@ -80,11 +72,15 @@ public class OS implements Comparable<OS> {
         };
 
         OS.printOs( osses );
+        
+        System.out.println();
         System.out.println( "Sorting Array in ascending order:" );
-        Arrays.sort( osses, new OSComparator() );
+        Arrays.sort( osses, cmpNatural );
+        
         System.out.println( "----------------" );
         System.out.println( "Sorted OS:" );
         System.out.println( "----------------" );
+        
         OS.printOs( osses );
 
         int key = Arrays.binarySearch( osses, new OS( "Linux", 1 ) );
@@ -95,13 +91,15 @@ public class OS implements Comparable<OS> {
 
         System.out.println();
 
-        Arrays.sort( osses, new OSComparator().reversed() );
+        Arrays.sort( osses, cmpNatural.reversed() );
+        
         System.out.println( "----------------" );
         System.out.println( "Reversed sorted OS:" );
         System.out.println( "----------------" );
         OS.printOs( osses );
+        
         System.out.println();
-        int key2 = Arrays.binarySearch( osses, new OS( "Windows", 95 ), new OSComparator().reversed() );
+        int key2 = Arrays.binarySearch( osses, new OS( "Windows", 95 ), cmpNatural.reversed() );
         if ( key2 >= 0 ) {
             System.out.println( "Found \"Windows 95 \" at position: " + key2 );
         }
