9948fa6e973ca8543207360df063df3eea33e6bf
Thomas <thopx@proton.me>
2022-08-04 14:52:51 +0200

    Aufgabe: 'List - Iterable' gelöst + ein paar Tests mit Generics und der CollectionAPI

---
+++ src/collectionapi/Tests.java
@@ -0,0 +1,42 @@
+package collectionapi;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Tests {
+
+	public static void main(String[] args) {
+		
+		Collection<Integer> myColl = new ArrayList<>();
+		
+		myColl.add(12);
+		myColl.add(17);
+		myColl.add(22);
+		
+		Integer[] arr = myColl.toArray(new Integer[myColl.size()]);
+		
+		for (Integer integer : arr) {
+			System.out.println(integer);
+			/*
+			 * 	12
+				17
+				22
+			 */
+		}
+		
+		System.out.println();
+		
+		Object[] arr2 = myColl.toArray();
+		
+		for (Object object : arr2) {
+			System.out.println(object.getClass());
+			/*
+			 * 	class java.lang.Integer
+				class java.lang.Integer
+				class java.lang.Integer
+			 */
+		}
+
+	}
+
+}
---
+++ src/generics/Tests.java
@@ -0,0 +1,24 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+class C1 { }
+class C2 extends C1 { }
+
+public class Tests {
+
+	public static void main(String[] args) {
+		
+		Collection<C1> myColl = new ArrayList<>();
+		
+		myColl.add(new C1());
+		myColl.add(new C2());
+//		myColl.add(new Integer(12)); // cf!
+		
+		myColl.remove(new C1());
+		myColl.remove(new C2());		
+
+	}
+
+}
