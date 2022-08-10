821dbca556c21f00f1d4f8dbed161faa83e7eb9f
Thomas <thopx@proton.me>
2022-08-10 09:11:39 +0200

    Aufgabe 'TreeSet - Integer' angepasst. Sortierreihenfolge 'absteigend'. Unittests ebenfalls angepasst.

--- src/main/java/collectionapi/aufgaben/MyLittleTreeSet.java
+++ src/main/java/collectionapi/aufgaben/MyLittleTreeSet.java
@@ -1,18 +1,20 @@
 package collectionapi.aufgaben;
 
+import java.util.Comparator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class MyLittleTreeSet {
 
-    public static SortedSet<Integer> randomTreeSet( int fromIncl, int toExcl, int howMayToSave ) {
-        SortedSet<Integer> set = new TreeSet<>();
+    public static SortedSet<Integer> randomTreeSet( int fromIncl, int toExcl, int howMayToSave,
+                                                    Comparator<Integer> comp ) {
+        SortedSet<Integer> set = new TreeSet<>( comp );
 
         for ( int i = 0; i < howMayToSave; i++ ) {
             int randomNr = ( int ) ( fromIncl + Math.round( Math.random() * ( toExcl - fromIncl ) ) );
 
             /*
-             Add only unique numbers. To have at least 100 elements in the list then.
+             Nur einzigartige Elemente hinzufügen.
              */
             while ( ! set.add( randomNr ) ) { // 'true' if the element is not in the list, 'false' otherwise!
                 randomNr = ( int ) ( fromIncl + Math.round( Math.random() * ( toExcl - fromIncl ) ) );
@@ -22,21 +24,31 @@ public class MyLittleTreeSet {
         return set;
     }
 
-    public static <T> SortedSet<T> getSubSetOf( SortedSet<T> set, T fromIncl, T toExcl ) {
+    public static <T> SortedSet<T> getSubSetOf( SortedSet<T> set, T fromIncl, T toExcl, Comparator<T> cmp ) {
         if ( fromIncl == null || toExcl == null ) {
             throw new IllegalArgumentException( "fromIncl and/or toExcl cannot be null!" );
+        }
+
+        if ( cmp.equals( Comparator.reverseOrder() ) ) {
+            /* Die Reihenfolge muss hier vertauscht werden, bei 'Comparator.reverseOrder()' */
+            return set.subSet( toExcl, fromIncl );
         }
 
         return set.subSet( fromIncl, toExcl );
     }
 
     public static void main( String[] args ) {
-        SortedSet<Integer> myRandomSet = randomTreeSet( 0, 1001, 100 );
+        SortedSet<Integer> myRandomSet = randomTreeSet( 0, 1001, 100, Comparator.reverseOrder() );
         System.out.println( myRandomSet );
-        
+
         System.out.println();
 
-        SortedSet<Integer> mySubset = getSubSetOf( myRandomSet, 800, 901 );
+        SortedSet<Integer> mySubset = getSubSetOf( myRandomSet, 799, 900, Comparator.reverseOrder() );
         System.out.println( mySubset );
+
+        mySubset.removeIf( i -> i > 800 );
+
+        System.out.println( mySubset );     // Leere Liste
+        System.out.println( myRandomSet );  // Liste wurde nicht angetastet!
     }
 }
--- src/test/java/MyLittleTreeSetTests.java
+++ src/test/java/MyLittleTreeSetTests.java
@@ -1,43 +1,57 @@
 import collectionapi.aufgaben.MyLittleTreeSet;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Comparator;
 import java.util.SortedSet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyLittleTreeSetTests {
 
+    static SortedSet<Integer> myRandomSet;
+
+    @BeforeAll
+    static void init() {
+        myRandomSet = MyLittleTreeSet.randomTreeSet( 0, 1001, 100, Comparator.reverseOrder() );
+    }
+
     @Test
     void generateRandomTreeSetWith100Values() {
-        int from = 0;
-        int to = 901;
-        int howMayTimes = 100;
-        SortedSet<Integer> myRandomSet = MyLittleTreeSet.randomTreeSet( to, from, howMayTimes );
-
         for ( Integer integer : myRandomSet ) {
-            assertTrue( integer >= from && integer <= to, "Integer value = " + integer );
+            assertTrue( integer >= 0 && integer <= 1000, "Integer value = " + integer );
         }
     }
 
     @ParameterizedTest
     @ValueSource(ints = { 100, 500 })
     void testIfSizeIsExactlyAsGiven( int size ) {
-        SortedSet<Integer> myRandomSet = MyLittleTreeSet.randomTreeSet( 0, 1000, size );
-
+        myRandomSet = MyLittleTreeSet.randomTreeSet( 0, 1001, size, Comparator.reverseOrder() );
         assertEquals( size, myRandomSet.size() );
     }
 
     @Test
     void testGetSubset() {
-        SortedSet<Integer> myRandomSet = MyLittleTreeSet.randomTreeSet( 0, 1000, 100 );
-        SortedSet<Integer> subSet = myRandomSet.subSet( 800, 901 ); // 800 incl. 901 excl. -> [ 800 .. 900 ]
+        SortedSet<Integer> subSet = myRandomSet.subSet( 900, 799 ); // 799 excl. 900 incl. -> reversedOrder() !
 
         for ( Integer integer : subSet ) {
             assertTrue( integer >= 800 && integer <= 900, "Integer value = " + integer ); // [ 800 .. 900 ]
         }
+    }
+
+    @Test
+    void testDescendingOrder() {
+        Object[] objects = myRandomSet.toArray();
+
+        for ( int i = 0; i < objects.length; i++ ) {
+            int j = i + 1;
+            if ( j < objects.length ) {
+                assertFalse( ( int ) objects[i] <= ( int ) objects[j], objects[i] + " <= " + objects[j] );
+            }
+        }
+
     }
 
 }
