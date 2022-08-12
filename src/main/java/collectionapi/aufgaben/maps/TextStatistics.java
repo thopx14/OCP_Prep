9bbf6eb50a55def4bd147041a650b3b34a0d9d44
Thomas <thopx@proton.me>
2022-08-12 09:53:48 +0200

    getUniqueChars() in TextStatistics.java angepasst. Algorithmus ein bisschen vereinfacht

--- src/main/java/collectionapi/aufgaben/maps/TextStatistics.java
+++ src/main/java/collectionapi/aufgaben/maps/TextStatistics.java
@@ -20,10 +20,15 @@ public class TextStatistics {
     }
 
     public Collection<Character> getUniqueChars() {
-        return text.chars().mapToObj( s -> ( char ) s ).collect( Collectors.toCollection( HashSet<Character>::new ) );
+        return text.chars().mapToObj( s -> (char) s )
+                .collect( Collectors.toCollection( HashSet<Character>::new ) );
     }
 
     public Map<Character, Integer> getCharCounts() {
+        /*
+         * Override HashMap class to get a better toString() output.
+         */
+        @SuppressWarnings("serial")
         Map<Character, Integer> charCountMap = new HashMap<>() {
             @Override
             public String toString() {
@@ -33,11 +38,11 @@ public class TextStatistics {
 
                 BiConsumer<Character, Integer> bc = ( k, v ) -> {
                     sb.append( "'" )
-                            .append( k )
-                            .append( "'" )
-                            .append( "=" )
-                            .append( get( k ) )
-                            .append( "; " );
+                    .append( k )
+                    .append( "'" )
+                    .append( "=" )
+                    .append( get( k ) )
+                    .append( "; " );
                 };
 
                 this.forEach( bc );
@@ -47,20 +52,21 @@ public class TextStatistics {
                 return sb.toString();
             }
         };
-        // Fill the map with '0'
-        for ( int i = 0; i < this.text.length(); i++ ) {
-            charCountMap.put( this.text.charAt( i ), 0 );
-        }
 
         // Count the chars
-        for ( int i = 0; i < this.text.length(); i++ ) {
-            int actualCnt = charCountMap.get( this.text.charAt( i ) );
-            charCountMap.put( this.text.charAt( i ), ++actualCnt );
+        for ( int i = 0; i < text.length(); i++ ) {
+            
+            Integer cnt = charCountMap.get( text.charAt( i ) );            
+            if ( cnt == null ) {
+                cnt = 0;
+            }
+            charCountMap.put( text.charAt( i ), ++cnt );
         }
         return charCountMap;
     }
 
     public static void main( String[] args ) {
+        
         TextStatistics stat = TextStatistics.of( "Heute ist Montag!" );
         Collection<Character> chars = stat.getUniqueChars();
         System.out.println( chars ); // [ , a, !, e, g, H, i, M, n, o, s, t, u]
@@ -68,6 +74,5 @@ public class TextStatistics {
         System.out.println( stat.getCharCounts() );
         // {' '=2; 'a'=1; '!'=1; 'e'=2; 'g'=1; 'H'=1; 'i'=1; 'M'=1; 'n'=1; 'o'=1; 's'=1; 't'=3; 'u'=1; }
     }
-
 
 }
