efc0ae148a93b6ae669aac11bec6e248550af188
Thomas <thopx@proton.me>
2022-08-24 18:08:03 +0200

    Kommentar zum Unit-Test hinzugefügt.

--- src/main/java/streams/aufgaben/StreamCountMinMax.java
+++ src/main/java/streams/aufgaben/StreamCountMinMax.java
@@ -6,6 +6,9 @@ import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * see StreamCountMinMaxTests.java for unit tests!
+ */
 public class StreamCountMinMax {
 
     private static final Locale[] locales = Locale.getAvailableLocales();
