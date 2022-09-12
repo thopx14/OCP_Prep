60a614684a6f3f0d81ff68969c19e8625bf8ec66
Thomas <thopx@proton.me>
2022-09-12 10:12:23 +0200

    Klasse ExecutorServiceSimple.java vereinfacht und ThreadLocaLRandom.current() anstatt new Random().. verwendet.

--- src/main/java/threads/aufgaben/ExecutorServiceSimple.java
+++ src/main/java/threads/aufgaben/ExecutorServiceSimple.java
@@ -3,9 +3,7 @@ package threads.aufgaben;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.*;
-import java.util.stream.Stream;
 
 public class ExecutorServiceSimple {
     public static void main( String[] args ) throws InterruptedException, ExecutionException {
@@ -20,11 +18,11 @@ public class ExecutorServiceSimple {
         //A3
         Collection<Callable<Integer>> callables = new ArrayList<>();
         for ( int i = 0; i < 100; i++ ) {
-            callables.add( () -> new Random().nextInt( 100 ) );
+            callables.add( () -> ThreadLocalRandom.current().nextInt( 100 ) );
         }
         List<Future<Integer>> futureList = service.invokeAll( callables, 10, TimeUnit.SECONDS );
-        int sumOfFutures = getSumOfFutureList( futureList );
-        System.out.println( "Sum of futures: " + sumOfFutures );
+        Future<Integer> resultInt = service.submit( () -> getSumOfFutureList( futureList ) );
+        System.out.println( "Sum of futures: " + resultInt.get() );
 
         service.shutdown();
     }
@@ -38,17 +36,14 @@ public class ExecutorServiceSimple {
     }
 
     static void printRandomDoubles() {
-        double randomSeedVal = new Random().nextDouble() * new Random().nextInt( 100 );
-        Stream.iterate( randomSeedVal, s -> new Random().nextDouble() * randomSeedVal )
+        ThreadLocalRandom.current().doubles()
                 .limit( 10 )
                 .forEach( System.out::println );
     }
 
     static double getRandomDoubles() {
-        double randomSeedVal = new Random().nextDouble() * new Random().nextInt( 100 );
-        return Stream.iterate( randomSeedVal, s -> new Random().nextDouble() * randomSeedVal )
+        return ThreadLocalRandom.current().doubles()
                 .limit( 10 )
-                .mapToDouble( d -> d )
                 .sum();
     }
 }
